3baac6bb441d0c07f2eaf98ed57baca15cd29891
ice-t <icet7523@gmail.com>
2020-03-01 23:24:34 +0300

    TODO added JSON added

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 $(document).ready(function(){
     $(".grid").kendoGrid(
-
+        /*
+        * TODO: Create template for columns with columns field and name from documnentation
+        * TODO: (dataSource) transport??? what is it and what it will do?
+        * TODO: flags (sortable, scroll e.t.c) documentation what they will do?
+        * TODO: data for dataSource  (at start from previous project)
+        * */
     )
-})
+});
