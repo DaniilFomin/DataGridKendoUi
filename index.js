b0c6b50dd543eda3ba5a50c86f11536618ef1ac6
ice-t <icet7523@gmail.com>
2020-03-18 17:04:58 +0300

    Date sort added

--- index.js
+++ index.js
@@ -1,24 +1,18 @@
 $(document).ready(function(){
     $(".grid").kendoGrid({
-        /*
-        * TODO: Create template for columns with columns field and name from documentation
-        * TODO: (dataSource) transport??? what is it and what it will do?
-        * TODO: flags (sortable, scroll e.t.c) documentation what they will do?
-        * TODO: data for dataSource  (at start from previous project)
-        * */
-        /*
-*
-* TODO: 1. Создать массив из 10 элементов
-* TODO: 2. положить массив в таблицу
-* TODO: 3. таблица рендерится
-* TODO: 4. через 100 секунд я удаляю из массива 2 записи, добавляю 3 других записи и изменяю в одной записи значения колонок
-* TODO: 5. табличка должна отразить изменения без дополнительных обращений к таблице
-*
-*/
+
        columns: [
-           {field:"name"},
+           {field:"name",},
            {field:"surname"},
-           {field:"year"},
+           {field:"year",
+               sortable: {
+                   compare: function(a, b) {
+                       return (new Date(a.year)) - (new Date(b.year));
+                   }
+               },
+               template: function (data) {
+               return kendo.htmlEncode(new Date(data.year).toDateString())
+           }},
            {field:"gender"},
        ],
         filterable: true,
